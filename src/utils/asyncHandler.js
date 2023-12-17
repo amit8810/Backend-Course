84ac0b97b5fcbcd140e2f94a1a59c0a6e0794f0b
amit8810 <ac446643@gmail.com>
2023-12-17 18:51:57 +0530

    utils code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,41 @@
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ANOTHER WAY TO WRITE
+// const asyncHandler = () => {}
+// const asyncHandler = (fn) => () => {}
+// const asyncHandler = (fn) => async () => {}
+/*
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message
+        })
+    }
+}
+*/
