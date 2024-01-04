6b78efbb9aa70dc748807b8711dbfacde104a11a
amit8810 <ac446643@gmail.com>
2024-01-04 22:17:34 +0530

    some bug fixed in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -235,7 +235,7 @@ const changeCurrentPassword = asyncHandler(async(req, res) => {
 const getCurrentUser = asyncHandler(async (req, res)=> {
     return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully")
+    .json(new ApiResponse(200, req.user, "current user fetched successfully"))
 })
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
@@ -268,6 +268,8 @@ const updateUserAvatar = asyncHandler(async (req, res)=> {
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is missing")
     }
+
+    //TODO: delete old image - assignment
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
 
